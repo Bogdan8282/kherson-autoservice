0e31dcaf8fa577f354ac030bbdb3b0a59aa0e787
Богдан Матрофайло <144223853+Bogdan8282@users.noreply.github.com>
2024-09-30 18:00:32 +0300

    Configured interactive elements

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
-document.querySelector('.intro__button').addEventListener('click', function() {
-  document.querySelector('.benefits').scrollIntoView({ behavior: 'smooth' });
+document.querySelector(".intro__button").addEventListener("click", function () {
+  document.querySelector(".benefits").scrollIntoView({ behavior: "smooth" });
+});
+
+document.querySelectorAll(".phone").forEach(function (phoneElement) {
+  phoneElement.addEventListener("click", function () {
+    var tempInput = document.createElement("input");
+    tempInput.value = "0988628268";
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
+
+    var notification = document.getElementById("notification");
+    notification.classList.add("show");
+
+    setTimeout(function () {
+      notification.classList.remove("show");
+    }, 2000);
+  });
 });
